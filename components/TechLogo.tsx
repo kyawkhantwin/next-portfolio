df2b61dd8cf791967c26e5d088bd4021c7309555
User <user@MMSs-MacBook-Air.local>
2024-08-28 20:26:27 +0630

    ok p d ta ku mr

--- components/TechLogo.tsx
+++ components/TechLogo.tsx
@@ -1,11 +1,16 @@
 import { Image } from "@nextui-org/image";
 import React from "react";
 
-const TechLogo = ({logo,name}) => {
+interface Tech {
+  logo: string;
+  name: string;
+}
+
+const TechLogo: React.FC<Tech> = ({ logo, name }) => {
   return (
-    <div className="flex gap-3 mt-4 flex-col ">
+    <div className="flex gap-3 mt-4 flex-col">
       <p>{name}</p>
-      <Image width={60} height={60} src={"/logo/"+logo} alt={name} />
+      <Image width={60} height={60} src={`/logo/${logo}`} alt={name} />
     </div>
   );
 };
