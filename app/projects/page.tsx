b739f2896f61f136af979c2f3886aa8e106625cf
User <user@MMSs-MacBook-Air.local>
2024-09-02 20:02:23 +0630

    all set

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,12 +10,16 @@ export default function DocsPage() {
           React Native FullStack (Web+Mobile)
         </h1>
 
+        <p>Clik View To See Mobile app </p>
+
         <Project
+        link="https://expo.dev/preview/update?message=added%20url%20%26%20real%20time%20chat&updateRuntimeVersion=1.0.0&createdAt=2024-09-02T13%3A31%3A44.680Z&slug=exp&projectId=335962c1-67de-4e7c-bd1c-ed8ab4005fa9&group=a700f240-0291-4f1c-ac7a-beb2ebf6e535"
           name={"Real Time Chatting Feature (Updated)"}
           url="/project/chatnative.png"
           slug={"chat"}
         />
         <Project
+        link="https://expo.dev/preview/update?message=done%20all%20i%20think&updateRuntimeVersion=1.0.0&createdAt=2024-08-30T15%3A58%3A44.197Z&slug=exp&projectId=8f98f760-fe52-4a0d-8212-8d23831e3cb7&group=98225e9f-789f-4975-a49c-2e0a3cbb68ca"
           name={"Ecommerce (Updated)"}
           url="/project/ecommercenative.png"
           slug={"ecommerce-native"}
