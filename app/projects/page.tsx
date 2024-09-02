403a749307fa4cdc8d83304136491e9e39d04e68
User <user@MMSs-MacBook-Air.local>
2024-09-02 20:05:34 +0630

    fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,9 +9,6 @@ export default function DocsPage() {
         <h1 className={subtitle({ bold: true })}>
           React Native FullStack (Web+Mobile)
         </h1>
-
-        <p>Clik View To See Mobile app </p>
-
         <Project
         link="https://expo.dev/preview/update?message=added%20url%20%26%20real%20time%20chat&updateRuntimeVersion=1.0.0&createdAt=2024-09-02T13%3A31%3A44.680Z&slug=exp&projectId=335962c1-67de-4e7c-bd1c-ed8ab4005fa9&group=a700f240-0291-4f1c-ac7a-beb2ebf6e535"
           name={"Real Time Chatting Feature (Updated)"}
@@ -25,12 +22,15 @@ export default function DocsPage() {
           slug={"ecommerce-native"}
         />
         <p>
+        <p><span className="text-red-500 font-bold">Note:</span> Clik View To See Mobile app </p>
+
           <span className="text-red-500 font-bold">Note:</span> Vercel only
           provides HTTPS servers; therefore, my HTTP backend server won&apos;t
           work.
           <br />
           As a result, I can&apos;t provide a version of the website that uses
           HTTP backend.
+
         </p>
       </div>
       <div className="">
